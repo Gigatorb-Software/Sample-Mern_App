4b95f792b2959728f0aff1e710d9b0b738a01803
neerajsharma1105 <sharmaneeraj9595@gmail.com>
2022-05-24 19:43:40 +0530

    further changes

--- mern_crud/server/app.js
+++ mern_crud/server/app.js
@@ -2,18 +2,27 @@ require("dotenv").config();
 const express= require("express")
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("./db/conn");
 
 const users = require("./models/userSchema");
 const cors = require("cors");
 const router = require("./routes/router");
-const port = 8003;
+
+app.set("port", process.env.PORT || 8003);
+console.log("+++++++++++++++++++++++" + app.get("port"));
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(express.static('../build'));
 
-app.listen(port,()=>{
-    console.log(`server is start port ${port}`);
+app.listen(process.env.PORT || 8003,()=>{
+    console.log(`server is start port ${process.env.PORT || 8003}`);
 })
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
